6da7777918af7531eb2463dd15695995f25e2263
Roberto Zambrano <rzajr868@gmail.com>
2023-12-19 18:24:12 +0100

    creación de boton de camara para acceder a ella y ponerla en la imageview

--- app/src/main/java/com/example/app_externa_gallery/MainActivity.java
+++ app/src/main/java/com/example/app_externa_gallery/MainActivity.java
@@ -3,17 +3,24 @@ package com.example.app_externa_gallery;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     ActivityResultLauncher<Intent> someActivityResultLauncher;
@@ -22,28 +29,79 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b = findViewById(R.id.accessgallery);
+
+        Button buttonGallery = findViewById(R.id.gallery);
+        Button buttonCamera = findViewById(R.id.camera);
+
         someActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult(ActivityResult result) {
                 if (result.getResultCode() == Activity.RESULT_OK) {
-                    // There are no request codes
                     Intent data = result.getData();
-                    Uri uri = data.getData();
-                    ImageView imageView = findViewById(R.id.imageView);
-                    imageView.setImageURI(uri);
+                    if (data != null) {
+                        Uri uri = data.getData();
+
+                        if (uri != null) {
+                            // La imagen proviene de la galería u otra fuente
+                            // Convierte la imagen de la galería a un Bitmap
+                            Bitmap bitmap;
+                            try {
+                                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                return;
+                            }
+
+                            // Actualiza el ImageView con el Bitmap
+                            ImageView imageView = findViewById(R.id.imageView);
+                            imageView.setImageBitmap(bitmap);
+                        } else {
+                            // La imagen proviene de la cámara
+                            Bundle extras = data.getExtras();
+                            if (extras != null) {
+                                Bitmap bitmap = (Bitmap) extras.get("data");
+                                // Actualiza el ImageView con el Bitmap
+                                ImageView imageView = findViewById(R.id.imageView);
+                                imageView.setImageBitmap(bitmap);
+                            } else {
+                                // Handle null extras
+                            }
+                        }
+                    }
                 }
             }
         });
-        b.setOnClickListener(new View.OnClickListener() {
+
+        buttonGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Open gallery
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("image/*");
                 intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-                //Launch activity to get result
                 someActivityResultLauncher.launch(intent);
             }
         });
+
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                someActivityResultLauncher.launch(takePictureIntent);
+            }
+        });
+    }
+
+    private String getRealPathFromUri(Uri uri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String filePath = cursor.getString(columnIndex);
+            cursor.close();
+            return filePath;
+        }
+        return null;
     }
 }
